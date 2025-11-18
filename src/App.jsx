98f23da0e30930997c07eb81e32907c2d3aa5414
Olga Muñoz <lm4109080@gmail.com>
2025-11-18 10:46:49 -0500

    Modificacion archivo app.jsx quintando confiraciones innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GameCard from './GameCard';
 
-function App() {
+const GameLibrary = ({ games }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-library">
+      <h2>Mi Biblioteca de Juegos</h2>
+      <div className="games-grid">
+        {games.map(game => (
+          <GameCard key={game._id} game={game} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default GameLibrary;
