778fe8abb89b552ca1a3fe4852f874823aed31bc
Olga Muñoz <lm4109080@gmail.com>
2025-11-18 11:33:11 -0500

    Agregar footer con información del proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>OLGA LUCIA MUÑOZ QUINTERO - PROYECTO JOVENES CREATIVO 2025 CALI</p>
+    </footer>
+  );
+};
+
+export default Footer;
